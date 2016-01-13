9e298df4083819f9fdc7abf4b313a19780e93011
Ronald Loyko <ronald.loyko@gmail.com>
2016-01-13 23:48:00 +0100

    Added unit test for Gambio API instantiation.

---
+++ test/instantiation.js
@@ -0,0 +1,21 @@
+const expect = require('chai').expect;
+
+const GambioApi = require('../.');
+const demoCredentials = require('../lib/demo/credentials');
+
+describe('Instantiation', () => {
+  it('should throw error on instantiating without credentials', () => {
+    const func = () => new GambioApi();
+    expect(func).to.throw(Error);
+  });
+
+  it('should throw error on instantiating with invalid credentials', () => {
+    const func = () => new GambioApi({ a: 'b' });
+    expect(func).to.throw(Error);
+  });
+
+  it('should work on instantiating with valid credentials', () => {
+    const func = () => new GambioApi(demoCredentials);
+    expect(func).not.to.throw(Error);
+  });
+});
