fc23ffc86de6c39e96795833be153d10c081b264
Ronald Loyko <ronald.loyko@gmail.com>
2016-01-15 14:16:52 +0100

    Added new demo directory.
    - New location is the root directory.

---
+++ demo/credentials.js
@@ -0,0 +1,11 @@
+/**
+ * Gambio demo shop 1 credentials.
+ *
+ * @see https://www.gambio-shop.de/shop1
+ * @type {object}
+ */
+module.exports = {
+  url: 'http://www.gambio-shop.de/shop1',
+  user: 'admin@shop.de',
+  pass: '12345',
+};
