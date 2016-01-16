d484536d3c41ce68e832e72be4fabb0585ace707
Ronald Loyko <ronald.loyko@gmail.com>
2016-01-16 17:45:26 +0100

    Changed Requester parameter list.

--- test/RequesterTest.js
+++ test/RequesterTest.js
@@ -1,4 +1,5 @@
 const expect = require('chai').expect;
+const extend = require('extend');
 const Promise = require('bluebird');
 const Requester = require('../lib/Requester');
 const demoCredentials = require('../demo/credentials');
@@ -9,14 +10,12 @@ const RequestError = require('../lib/error/RequestError');
 const ResourceNotFoundError = require('../lib/error/ResourceNotFoundError');
 
 // Test credentials.
-const testUrl = `${demoCredentials.url}/api.php/v2`;
-const testUser = demoCredentials.user;
-const testPassword = demoCredentials.pass;
+const credentials = extend(true, {}, demoCredentials, { url: `${demoCredentials.url}/api.php/v2` });
 
 describe('Requester', () => {
   describe('#constructor', () => {
     it('should work when valid parameters has been passed', () => {
-      const func = () => new Requester(testUrl, testUser, testPassword);
+      const func = () => new Requester(credentials);
       expect(func).not.to.throw(Error);
     });
 
@@ -26,45 +25,73 @@ describe('Requester', () => {
     });
 
     it('should throw NoArgumentError on missing password', () => {
-      const func = () => new Requester(testUrl, testUser);
+      const crx = {
+        url: credentials.url,
+        user: credentials.user,
+      };
+      const func = () => new Requester(crx);
       expect(func).to.throw(NoArgumentError);
     });
 
     it('should throw InvalidArgumentError on wrong type of password', () => {
-      const func = () => new Requester(testUrl, testUser, 123123);
+      const crx = {
+        url: credentials.url,
+        user: credentials.user,
+        pass: 123,
+      };
+      const func = () => new Requester(crx);
       expect(func).to.throw(InvalidArgumentError);
     });
 
     it('should throw NoArgumentError on missing user', () => {
-      const func = () => new Requester(testUrl);
+      const crx = {
+        url: credentials.url,
+        pass: credentials.pass,
+      };
+      const func = () => new Requester(crx);
       expect(func).to.throw(NoArgumentError);
     });
 
     it('should throw InvalidArgumentError on wrong type of user', () => {
-      const func = () => new Requester(testUrl, 123123);
+      const crx = {
+        url: credentials.url,
+        user: 123,
+        pass: credentials.pass,
+      };
+      const func = () => new Requester(crx);
       expect(func).to.throw(InvalidArgumentError);
     });
 
     it('should throw NoArgumentError on missing URL', () => {
-      const func = () => new Requester();
+      const crx = {
+        user: credentials.user,
+        pass: credentials.pass,
+      };
+      const func = () => new Requester(crx);
       expect(func).to.throw(NoArgumentError);
     });
 
     it('should throw InvalidArgumentError on wrong type of URL', () => {
-      const func = () => new Requester(123123);
+      const crx = {
+        user: credentials.user,
+        pass: credentials.pass,
+        url: 22,
+      };
+      const func = () => new Requester(crx);
       expect(func).to.throw(InvalidArgumentError);
     });
   });
 
   describe('#get', () => {
     it('should return a promise', () => {
-      const request = new Requester(testUrl, testUser, testPassword);
+      const request = new Requester(credentials);
       const get = request.get();
       expect(get).to.be.an.instanceOf(Promise);
     });
 
     it('should return rejected promise with AuthenticationError on invalid credentials', (done) => {
-      const request = new Requester(testUrl, '', '');
+      const crx = extend(true, {}, credentials, { user: '', pass: '' });
+      const request = new Requester(crx);
       const get = request.get();
 
       get.catch((error) => {
@@ -74,14 +101,11 @@ describe('Requester', () => {
     });
 
     it('should return rejected promise with RequestError while performing request', (done) => {
-      const url = 'http://127.0.0.2';
-      const request = new Requester(url, '', '');
+      const crx = extend(true, {}, credentials, { url: 'http://127.0.0.2' });
+      const request = new Requester(crx);
       const get = request.get();
 
       get
-        .then(() => {
-          done();
-        })
         .catch((error) => {
           expect(error).to.be.instanceOf(RequestError);
           done();
@@ -89,14 +113,11 @@ describe('Requester', () => {
     });
 
     it('should return rejected promise with ResourceNotFoundError on 404 status code', (done) => {
-      const url = `${testUrl}/asiudgasoidzo78324`;
-      const request = new Requester(url, '', '');
+      const crx = extend(true, {}, credentials, { url: `${credentials.url}/asiudgasoidzo78324` });
+      const request = new Requester(crx);
       const get = request.get();
 
       get
-        .then(() => {
-          done();
-        })
         .catch((error) => {
           expect(error).to.be.instanceOf(ResourceNotFoundError);
           done();
@@ -104,7 +125,7 @@ describe('Requester', () => {
     });
 
     it('should return parsed object on successful request with custom headers', (done) => {
-      const request = new Requester(testUrl, testUser, testPassword);
+      const request = new Requester(credentials);
       const headers = { 'Content-Type': 'application/json' };
       const get = request.get(headers);
 
