5002f8e2c42d7f5a99db13df653f33125e6a73ea
r.loyko <r.loyko@gambio.de>
2016-03-02 14:54:54 +0100

    Refactored address provider.

--- src/provider/AddressProvider.js
+++ src/provider/AddressProvider.js
@@ -1,28 +1,16 @@
+import _ from 'lodash';
+import Provider from './Provider';
+
 /**
- * @name Address API
+ * Class representing a address provider.
  * @description Provides an API for adresses.
- * @example
- * 	API.addresses.getById(36)
- * 		.then(console.log)
- * 		.catch(console.error);
- */
-
-'use strict';
-
-const Api = require('./Api');
-const Requester = require('./../Requester');
-const Validator = require('./../Validator');
-
-/**
- * Class representing a Address API.
  * @extends Api
  */
-class AddressApi extends Api {
+class AddressProvider extends Provider {
+
   /**
    * Returns the API endpoint URL suffix.
-   *
    * @return {String}
-   * @override
    * @private
    */
   _getSuffix() {
@@ -31,88 +19,69 @@ class AddressApi extends Api {
 
   /**
    * Returns address by provided address ID.
-   *
    * @param {Number} id Address ID.
-   *
-   * @throws ArgumentNullError  If argument is missing.
-   * @throws ArgumentError      If argument is invalid.
-   *
+   * @throws {Error} On missing or invalid argument.
    * @return {Promise}
    */
   getById(id) {
-    // Check argument.
-    Validator.checkInteger(id, 'Address ID');
+    // Check ID.
+    if (_.isNil(id) || !_.isInteger(id)) {
+      throw new Error('ID is missing or invalid.');
+    }
 
-    // Compose URL.
-    const url = this._getEndpointUrl() + `/${id}`;
-
-    // Return request promise.
-    return Requester.get(url, this._getAuth());
+    return this.dispatcher.get(`${this._getEndpointUrl()}/${id}`);
   }
 
   /**
    * Creates a new address entry.
-   *
    * @param {Object} data Address data.
-   *
-   * @throws ArgumentNullError  If argument is missing.
-   * @throws ArgumentError      If argument is invalid.
-   *
+   * @throws {Error} On missing or invalid argument.
    * @return {Promise}
    */
   create(data) {
-    // Check argument.
-    Validator.checkObject(data, 'Address data');
+    // Check data.
+    if (_.isNil(data) || !_.isObject(data)) {
+      throw new Error('Data is missing or invalid.');
+    }
 
-    // Return request promise.
-    return Requester.post(this._getEndpointUrl(), this._getAuth(), data);
+    return this.dispatcher.post(`${this._getEndpointUrl()}`, data);
   }
 
   /**
    * Deletes address by provided address ID.
-   *
    * @param {Number} id Address ID.
-   *
-   * @throws ArgumentNullError  If argument is missing.
-   * @throws ArgumentError      If argument is invalid.
-   *
+   * @throws {Error} On missing or invalid argument.
    * @return {Promise}
    */
   deleteById(id) {
-    // Check argument.
-    Validator.checkInteger(id, 'Address ID');
+    // Check ID.
+    if (_.isNil(id) || !_.isInteger(id)) {
+      throw new Error('ID is missing or invalid.');
+    }
 
-    // Compose URL.
-    const url = this._getEndpointUrl() + `/${id}`;
-
-    // Return request promise.
-    return Requester.delete(url, this._getAuth());
+    return this.dispatcher.delete(`${this._getEndpointUrl()}/${id}`);
   }
 
   /**
    * Updates an address entry based on the address data and address ID provided.
-   *
    * @param {Number} id   Address ID.
    * @param {Object} data Address data.
-   *
-   * @throws ArgumentNullError  If an argument is missing.
-   * @throws ArgumentError      If an argument is invalid.
-   *
+   * @throws {Error} On missing or invalid argument.
    * @return {Promise}
    */
   updateById(id, data) {
-    // Check ID argument.
-    Validator.checkInteger(id, 'Address ID');
+    // Check ID.
+    if (_.isNil(id) || !_.isInteger(id)) {
+      throw new Error('ID is missing or invalid.');
+    }
 
-    // Check address data argument.
-    Validator.checkObject(data, 'Address data');
+    // Check data.
+    if (_.isNil(data) || !_.isObject(data)) {
+      throw new Error('Data is missing or invalid.');
+    }
 
-    // Compose URL.
-    const url = this._getEndpointUrl() + `/${id}`;
-
-    // Return request promise.
-    return Requester.put(url, this._getAuth(), data);
+    return this.dispatcher.put(`${this._getEndpointUrl()}/${id}`);
   }
 }
 
-module.exports = AddressApi;
+export default AddressProvider;
---
+++ test/provider/AddressProviderTest.js
@@ -0,0 +1,209 @@
+import { assert } from 'chai';
+import Promise from 'bluebird';
+import AddressProvider from './../../lib/provider/AddressProvider';
+import credentials from './../_credentials';
+import apiUrl from './../_apiUrl';
+import data from './../_data/address';
+
+describe('AddressProvider', () => {
+  // Valid URL.
+  const url = credentials.url + apiUrl;
+
+  // Valid authentication object.
+  const auth = { user: credentials.user, pass: credentials.pass };
+
+  // Valid instance.
+  const instance = new AddressProvider(url, auth);
+
+  describe('#getById', () => {
+    it('throws error on missing arguments', () => {
+      const sandbox = () => instance.getById();
+      assert.throws(sandbox, Error);
+    });
+
+    it('throws error on invalid argument', () => {
+      const sandbox = () => instance.getById(1.232);
+      assert.throws(sandbox, Error);
+    });
+
+    it('returns a promise', () => {
+      assert.instanceOf(instance.getById(1), Promise);
+    });
+
+    it('resolves the promise', (done) => {
+      // Creates a new item first.
+      instance
+        .create(data)
+        .then((result) => instance.getById(result.id))
+        .then(() => {
+          assert.ok('Fetching');
+          done();
+        })
+        .catch(() => {
+          assert.notOk('Fetching');
+          done();
+        });
+    });
+
+    it('rejects the promise', (done) => {
+      instance
+        .getById(999999)
+        .then(() => {
+          assert.notOk('Fetching');
+          done();
+        })
+        .catch(() => {
+          assert.ok('Fetching');
+          done();
+        });
+    });
+  });
+
+  describe('#create', () => {
+    it('throws error on missing arguments', () => {
+      const sandbox = () => instance.create();
+      assert.throws(sandbox, Error);
+    });
+
+    it('throws error on invalid argument', () => {
+      const sandbox = () => instance.create(1.232);
+      assert.throws(sandbox, Error);
+    });
+
+    it('returns a promise', () => {
+      assert.instanceOf(instance.create(data), Promise);
+    });
+
+    it('resolves the promise', (done) => {
+      instance
+        .create(data)
+        .then(() => {
+          assert.ok('Creating');
+          done();
+        })
+        .catch(() => {
+          assert.notOk('Creating');
+          done();
+        });
+    });
+
+    it('rejects the promise', (done) => {
+      instance
+        .create({})
+        .then(() => {
+          assert.notOk('Creating');
+          done();
+        })
+        .catch(() => {
+          assert.ok('Creating');
+          done();
+        });
+    });
+  });
+
+  describe('#deleteById', () => {
+    it('throws error on missing arguments', () => {
+      const sandbox = () => instance.deleteById();
+      assert.throws(sandbox, Error);
+    });
+
+    it('throws error on invalid argument', () => {
+      const sandbox = () => instance.deleteById(1.232);
+      assert.throws(sandbox, Error);
+    });
+
+    it('returns a promise', () => {
+      assert.instanceOf(instance.deleteById(9999999), Promise);
+    });
+
+    it('resolves the promise', (done) => {
+      // Creates a new item first.
+      instance
+        .create(data)
+        .then((result) => instance.deleteById(result.id))
+        .then(() => {
+          assert.ok('Removal');
+          done();
+        })
+        .catch(() => {
+          assert.notOk('Removal');
+          done();
+        });
+    });
+
+    it('rejects the promise', (done) => {
+      instance
+        .deleteById(999999999999999)
+        .then(() => {
+          assert.notOk('Removal');
+          done();
+        })
+        .catch(() => {
+          assert.ok('Removal');
+          done();
+        });
+    });
+  });
+
+  describe('#updateById', () => {
+    // Valid data object for update.
+    const updateData = { company: 'Example Inc.' };
+
+    it('throws error on missing arguments', () => {
+      const sandbox = () => instance.updateById();
+      assert.throws(sandbox, Error);
+    });
+
+    it('throws error on missing ID', () => {
+      const sandbox = () => instance.updateById(null, updateData);
+      assert.throws(sandbox, Error);
+    });
+
+    it('throws error on invalid ID', () => {
+      const sandbox = () => instance.updateById(1.2, updateData);
+      assert.throws(sandbox, Error);
+    });
+
+    it('throws error on missing data object', () => {
+      const sandbox = () => instance.updateById(1);
+      assert.throws(sandbox, Error);
+    });
+
+    it('throws error on invalid data object', () => {
+      const sandbox = () => instance.updateById(1, 1);
+      assert.throws(sandbox, Error);
+    });
+
+    it('returns a promise', () => {
+      assert.instanceOf(instance.updateById(9999999, updateData), Promise);
+    });
+
+    it('resolves the promise', (done) => {
+      // Creates a new item first.
+      instance
+        .create(data)
+        .then((result) => instance.updateById(result.id, updateData))
+        .then(() => {
+          assert.ok('Update');
+          done();
+        })
+        .catch(() => {
+          assert.notOk('Update');
+          done();
+        });
+    });
+
+    it('rejects the promise', (done) => {
+      instance
+        .updateById(999999999999999, updateData)
+        .then(() => {
+          assert.notOk('Removal');
+          done();
+        })
+        .catch(() => {
+          assert.ok('Removal');
+          done();
+        });
+    });
+  });
+});
