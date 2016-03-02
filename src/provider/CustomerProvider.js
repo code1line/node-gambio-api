3ae2fd0bb20fbe7e65d75bed9da021b3a2918f53
r.loyko <r.loyko@gambio.de>
2016-03-02 17:55:48 +0100

    Refactored CustomerProvider.

--- src/provider/CustomerProvider.js
+++ src/provider/CustomerProvider.js
@@ -1,28 +1,15 @@
+import _ from 'lodash';
+import Provider from './Provider';
+
 /**
- * @name Customer API
+ * Class representing a customer provider.
  * @description Provides an API for customers.
- * @example
- * 	API.customers.getById(36)
- * 		.then(console.log)
- * 		.catch(console.error);
+ * @extends Provider
  */
-
-'use strict';
-
-const Api = require('./Api');
-const Requester = require('./../Requester');
-const Validator = require('./../Validator');
-
-/**
- * Class representing a customer API.
- * @extends Api
- */
-class CustomerApi extends Api {
+class CustomerProvider extends Provider {
   /**
    * Returns the API endpoint URL suffix.
-   *
    * @return {String}
-   * @override
    * @private
    */
   _getSuffix() {
@@ -31,175 +18,115 @@ class CustomerApi extends Api {
 
   /**
    * Returns all customers.
-   * Optionally, it is possible to pass in sorting and fieldname limitation criteria.
-   *
-   * @param {Object}    [sorting] Sorting criteria.
-   * @param {String[]}  [limits]  Response fields limits.
-   *
-   * @example
-   * 	CustomerApi.get({ lastname: 'asc' }, ['id', 'firstname']);
-   *
    * @return {Promise}
    */
-  get(sorting, limits) {
-    // Check manipulator arguments and change URL if required.
-    const url = this._modifyUrlByManipulatorArguments(this._getEndpointUrl(), sorting, limits);
-
-    // Return request promise.
-    return Requester.get(url, this._getAuth());
+  get() {
+    return this.dispatcher.get(this._getEndpointUrl());
   }
 
   /**
    * Searches in customers.
-   *
-   * @param  {String} term Search term.
-   *
-   * @throws ArgumentNullError  If argument is missing.
-   * @throws ArgumentError      If argument is invalid.
-   *
+   * @param {String} term Search term.
+   * @throws {Error} On missing or invalid argument.
    * @return {Promise}
    */
   search(term) {
-    // Check argument.
-    Validator.checkString(term, 'Customers search term');
+    // Check search term.
+    if (_.isNil(term) || !_.isString(term)) {
+      throw new Error('Search term is missing or invalid.');
+    }
 
-    // Compose URL.
-    const url = this._getEndpointUrl() + `?q=${term}`;
-
-    // Return request promise.
-    return Requester.get(url, this._getAuth());
+    return this.dispatcher.get(`${this._getEndpointUrl()}?q=${term}`);
   }
 
   /**
    * Returns all guest customers.
-   * Optionally, it is possible to pass in sorting and fieldname limitation criteria.
-   *
-   * @param {Object}    [sorting] Sorting criteria.
-   * @param {String[]}  [limits]  Response fields limits.
-   *
    * @return {Promise}
    */
-  getGuests(sorting, limits) {
-    // URL parameters to be appended to URL.
-    const suffixParameters = ['type=guest'];
-
-    // Check manipulator arguments and change URL if required.
-    const url = this._modifyUrlByManipulatorArguments(
-      this._getEndpointUrl(),
-      sorting,
-      limits,
-      suffixParameters
-    );
-
-    // Return request promise.
-    return Requester.get(url, this._getAuth());
+  getGuests() {
+    return this.dispatcher.get(`${this._getEndpointUrl()}?type=guest`);
   }
 
   /**
    * Returns addresses by provided customer ID.
-   *
    * @param {Number} id Customer ID.
-   *
-   * @throws ArgumentNullError  If argument is missing.
-   * @throws ArgumentError      If argument is invalid.
-   *
+   * @throws {Error} On missing or invalid argument.
    * @return {Promise}
    */
   getAddressesByCustomerId(id) {
-    // Check argument.
-    Validator.checkInteger(id, 'Customer ID');
+    // Check ID.
+    if (_.isNil(id) || !_.isInteger(id)) {
+      throw new Error('ID is missing or invalid.');
+    }
 
-    // Compose URL.
-    const url = this._getEndpointUrl() + `/${id}/addresses`;
-
-    // Return request promise.
-    return Requester.get(url, this._getAuth());
+    return this.dispatcher.get(`${this._getEndpointUrl()}/${id}/addresses`);
   }
 
   /**
    * Returns customer by provided customer ID.
-   *
    * @param {Number} id Customer ID.
-   *
-   * @throws ArgumentNullError  If argument is missing.
-   * @throws ArgumentError      If argument is invalid.
-   *
+   * @throws {Error} On missing or invalid argument.
    * @return {Promise}
    */
   getById(id) {
-    // Check argument.
-    Validator.checkInteger(id, 'Customer ID');
+    // Check ID.
+    if (_.isNil(id) || !_.isInteger(id)) {
+      throw new Error('ID is missing or invalid.');
+    }
 
-    // Compose URL.
-    const url = this._getEndpointUrl() + `/${id}`;
-
-    // Return request promise.
-    return Requester.get(url, this._getAuth());
+    return this.dispatcher.get(`${this._getEndpointUrl()}/${id}`);
   }
 
   /**
    * Creates a new customer.
-   *
    * @param {Object} data Customer data.
-   *
-   * @throws ArgumentNullError  If argument is missing.
-   * @throws ArgumentError      If argument is invalid.
-   *
+   * @throws {Error} On missing or invalid argument.
    * @return {Promise}
    */
   create(data) {
-    // Check argument.
-    Validator.checkObject(data, 'Customer data');
+    // Check customer data.
+    if (_.isNil(data) || !_.isObject(data)) {
+      throw new Error('Customer data object is missing or invalid.');
+    }
 
-    // Return request promise.
-    return Requester.post(this._getEndpointUrl(), this._getAuth(), data);
+    return this.dispatcher.post(this._getEndpointUrl(), data);
   }
 
   /**
    * Deletes customer by provided customer ID.
-   *
    * @param {Number} id Customer ID.
-   *
-   * @throws ArgumentNullError  If argument is missing.
-   * @throws ArgumentError      If argument is invalid.
-   *
+   * @throws {Error} On missing or invalid argument.
    * @return {Promise}
    */
   deleteById(id) {
-    // Check argument.
-    Validator.checkInteger(id, 'Customer ID');
+    // Check ID.
+    if (_.isNil(id) || !_.isInteger(id)) {
+      throw new Error('ID is missing or invalid.');
+    }
 
-    // Compose URL.
-    const url = this._getEndpointUrl() + `/${id}`;
-
-    // Return request promise.
-    return Requester.delete(url, this._getAuth());
+    return this.dispatcher.delete(`${this._getEndpointUrl()}/${id}`);
   }
 
   /**
    * Updates a customer based on the customer data and customer ID provided.
-   *
    * @param {Number} id    Customer ID.
    * @param {Object} data  Customer data.
-   *
-   * @throws ArgumentNullError  If argument is missing.
-   * @throws ArgumentError      If argument is invalid.
-   *
+   * @throws {Error} On missing or invalid argument.
    * @return {Promise}
    */
   updateById(id, data) {
-    // Check ID argument.
-    Validator.checkInteger(id, 'Customer ID');
+    // Check ID.
+    if (_.isNil(id) || !_.isInteger(id)) {
+      throw new Error('ID is missing or invalid.');
+    }
 
-    // Check data argument.
-    Validator.checkObject(data, 'Customer data');
+    // Check customer data.
+    if (_.isNil(data) || !_.isObject(data)) {
+      throw new Error('Customer data object is missing or invalid.');
+    }
 
-    // Compose URL.
-    const url = this._getEndpointUrl() + `/${id}`;
-
-    // Return request promise.
-    return Requester.put(url, this._getAuth(), data);
+    return this.dispatcher.put(`${this._getEndpointUrl()}/${id}`, data);
   }
 }
 
-module.exports = CustomerApi;
+export default CustomerProvider;
---
+++ test/provider/CustomerProviderTest.js
@@ -0,0 +1,331 @@
+import { assert } from 'chai';
+import Promise from 'bluebird';
+import CustomerProvider from './../../lib/provider/CustomerProvider';
+import credentials from './../_credentials';
+import apiUrl from './../_apiUrl';
+import getData from './../_data/customer';
+
+describe('CustomerProvider', () => {
+  // Valid URL.
+  const url = credentials.url + apiUrl;
+
+  // Valid authentication object.
+  const auth = { user: credentials.user, pass: credentials.pass };
+
+  // Valid instance.
+  const instance = new CustomerProvider(url, auth);
+
+  describe('#get', () => {
+    it('returns a promise', () => {
+      assert.instanceOf(instance.get(), Promise);
+    });
+
+    it('resolves the promise', (done) => {
+      instance
+        .get()
+        .then(() => {
+          assert.ok('Fetching');
+          done();
+        })
+        .catch(() => {
+          assert.notOk('Fetching');
+          done();
+        });
+    });
+  });
+
+  describe('#search', () => {
+    it('throws error on missing arguments', () => {
+      const sandbox = () => instance.search();
+      assert.throws(sandbox, Error);
+    });
+
+    it('throws error on invalid argument', () => {
+      const sandbox = () => instance.search(1.232);
+      assert.throws(sandbox, Error);
+    });
+
+    it('returns a promise', () => {
+      assert.instanceOf(instance.search('test'), Promise);
+    });
+
+    it('resolves the promise', (done) => {
+      instance
+        .search('test')
+        .then(() => {
+          assert.ok('Fetching');
+          done();
+        })
+        .catch(() => {
+          assert.notOk('Fetching');
+          done();
+        });
+    });
+
+    it('rejects the promise', (done) => {
+      instance
+        .search('üüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüü')
+        .then(() => {
+          assert.notOk('Fetching');
+          done();
+        })
+        .catch(() => {
+          assert.ok('Fetching');
+          done();
+        });
+    });
+  });
+
+  describe('#getGuests', () => {
+    it('returns a promise', () => {
+      assert.instanceOf(instance.getGuests(), Promise);
+    });
+
+    it('resolves the promise', (done) => {
+      instance
+        .getGuests()
+        .then(() => {
+          assert.ok('Fetching');
+          done();
+        })
+        .catch(() => {
+          assert.notOk('Fetching');
+          done();
+        });
+    });
+  });
+
+  describe('#getAddressesByCustomerId', () => {
+    it('throws error on missing arguments', () => {
+      const sandbox = () => instance.getAddressesByCustomerId();
+      assert.throws(sandbox, Error);
+    });
+
+    it('throws error on invalid argument', () => {
+      const sandbox = () => instance.getAddressesByCustomerId(1.232);
+      assert.throws(sandbox, Error);
+    });
+
+    it('returns a promise', () => {
+      assert.instanceOf(instance.getAddressesByCustomerId(1), Promise);
+    });
+
+    it('resolves the promise', (done) => {
+      instance
+        .create(getData())
+        .then((result) => instance.getAddressesByCustomerId(result.id))
+        .then(() => {
+          assert.ok('Fetching');
+          done();
+        })
+        .catch(() => {
+          assert.notOk('Fetching');
+          done();
+        });
+    });
+
+    it('rejects the promise', (done) => {
+      instance
+        .getAddressesByCustomerId(99999999999999999999999999999999999)
+        .then(() => {
+          assert.notOk('Fetching');
+          done();
+        })
+        .catch(() => {
+          assert.ok('Fetching');
+          done();
+        });
+    });
+  });
+
+  describe('#getById', () => {
+    it('throws error on missing arguments', () => {
+      const sandbox = () => instance.getById();
+      assert.throws(sandbox, Error);
+    });
+
+    it('throws error on invalid argument', () => {
+      const sandbox = () => instance.getById(1.232);
+      assert.throws(sandbox, Error);
+    });
+
+    it('returns a promise', () => {
+      assert.instanceOf(instance.getById(1), Promise);
+    });
+
+    it('resolves the promise', (done) => {
+      instance
+        .create(getData())
+        .then((result) => instance.getById(result.id))
+        .then(() => {
+          assert.ok('Fetching');
+          done();
+        })
+        .catch(() => {
+          assert.notOk('Fetching');
+          done();
+        });
+    });
+
+    it('rejects the promise', (done) => {
+      instance
+        .getById(99999999999999999999999999999999999)
+        .then(() => {
+          assert.notOk('Fetching');
+          done();
+        })
+        .catch(() => {
+          assert.ok('Fetching');
+          done();
+        });
+    });
+  });
+
+  describe('#create', () => {
+    it('throws error on missing arguments', () => {
+      const sandbox = () => instance.create();
+      assert.throws(sandbox, Error);
+    });
+
+    it('throws error on invalid argument', () => {
+      const sandbox = () => instance.create(1.232);
+      assert.throws(sandbox, Error);
+    });
+
+    it('returns a promise', () => {
+      assert.instanceOf(instance.create(getData()), Promise);
+    });
+
+    it('resolves the promise', (done) => {
+      instance
+        .create(getData())
+        .then(() => {
+          assert.ok('Fetching');
+          done();
+        })
+        .catch(() => {
+          assert.notOk('Fetching');
+          done();
+        });
+    });
+
+    it('rejects the promise', (done) => {
+      instance
+        .create({})
+        .then(() => {
+          assert.notOk('Fetching');
+          done();
+        })
+        .catch(() => {
+          assert.ok('Fetching');
+          done();
+        });
+    });
+  });
+
+  describe('#deleteById', () => {
+    it('throws error on missing arguments', () => {
+      const sandbox = () => instance.deleteById();
+      assert.throws(sandbox, Error);
+    });
+
+    it('throws error on invalid argument', () => {
+      const sandbox = () => instance.deleteById(1.232);
+      assert.throws(sandbox, Error);
+    });
+
+    it('returns a promise', () => {
+      assert.instanceOf(instance.deleteById(99999999999), Promise);
+    });
+
+    it('resolves the promise', (done) => {
+      instance
+        .create(getData())
+        .then((result) => instance.deleteById(result.id))
+        .then(() => {
+          assert.ok('Fetching');
+          done();
+        })
+        .catch(() => {
+          assert.notOk('Fetching');
+          done();
+        });
+    });
+
+    it('rejects the promise', (done) => {
+      instance
+        .deleteById(99999999999999999999999999999999999)
+        .then(() => {
+          assert.notOk('Fetching');
+          done();
+        })
+        .catch(() => {
+          assert.ok('Fetching');
+          done();
+        });
+    });
+  });
+
+  describe('#updateById', () => {
+    // Valid update data.
+    const updateData = { firstname: 'Marcus' };
+
+    it('throws error on missing arguments', () => {
+      const sandbox = () => instance.updateById();
+      assert.throws(sandbox, Error);
+    });
+
+    it('throws error on missing ID', () => {
+      const sandbox = () => instance.updateById(null, getData());
+      assert.throws(sandbox, Error);
+    });
+
+    it('throws error on invalid ID', () => {
+      const sandbox = () => instance.updateById(1.23, getData());
+      assert.throws(sandbox, Error);
+    });
+
+    it('throws error on missing data object', () => {
+      const sandbox = () => instance.updateById(1);
+      assert.throws(sandbox, Error);
+    });
+
+    it('throws error on invalid data object', () => {
+      const sandbox = () => instance.updateById(1, 1);
+      assert.throws(sandbox, Error);
+    });
+
+    it('returns a promise', () => {
+      instance
+        .create(getData())
+        .then((result) => assert.instanceOf(instance.updateById(result.id, updateData), Promise));
+    });
+
+    it('resolves the promise', (done) => {
+      instance
+        .create(getData())
+        .then((result) => instance.updateById(result.id, updateData))
+        .then(() => {
+          assert.ok('Fetching');
+          done();
+        })
+        .catch(() => {
+          assert.notOk('Fetching');
+          done();
+        });
+    });
+
+    it('rejects the promise', (done) => {
+      instance
+        .updateById(99999999999999999999999999999999999, updateData)
+        .then(() => {
+          assert.notOk('Fetching');
+          done();
+        })
+        .catch(() => {
+          assert.ok('Fetching');
+          done();
+        });
+    });
+  });
+});
