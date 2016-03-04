8e8b091374c3058544784b40f118fa7ccc8cb7bd
r.loyko <r.loyko@gambio.de>
2016-03-04 11:49:54 +0100

    Removed old test fixture files.

--- test/_data/orderStatus.js
+++
@@ -1,6 +0,0 @@
-// Order status data.
-export default {
-  statusId: 1,
-  comment: 'This is a comment.',
-  customerNotified: false,
-};
--- test/_data/orderTotal.js
+++
@@ -1,8 +0,0 @@
-// Order total data.
-export default {
-  title: 'Zwischensumme:',
-  value: 50,
-  valueText: '50,00 EUR',
-  class: 'ot_subtotal',
-  sortOrder: 10,
-};
